7809ec65c3ec5f48a3adb7e472ba0bb81180eabe
ann.komova <ann.komova@gmail.com>
2023-02-22 01:58:50 +0300

    add front

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from "@nestjs/common";
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   const port = process.env.PORT || 3000;
   Logger.log("Application is running on: http://localhost:${port}");
   await app.listen(port);
