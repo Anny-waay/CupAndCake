41480ee58ac745123310301d350cbd1c5b80c449
ann.komova <ann.komova@gmail.com>
2023-03-15 12:27:59 +0300

    add partials and pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,202 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Render, UseInterceptors } from "@nestjs/common";
 import { AppService } from './app.service';
+import {TimeLoadingInterceptor } from "./time.loading.interceptor";
 
+@UseInterceptors(TimeLoadingInterceptor)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  index() {
+    return {
+      title: 'Cup&Cake',
+      isIndex: true,
+      specialOffers: [
+        {
+          image:"images/coffee_cake.jpeg",
+          product:"Кофе арабика + торт \"Лесные ягоды\"",
+          prevPrice: 3000,
+          actualPrice: 1999
+        },
+        {
+          image:"images/cupcakes_helloween.jpeg",
+          product:"Капкейки Хэллоуин",
+          prevPrice: 2000,
+          actualPrice: 1499
+        },
+        {
+          image:"images/tea_cake.jpg",
+          product:"Чай \"Эрл Грей\" + Лимонный тарт",
+          prevPrice: 2500,
+          actualPrice: 1799
+        }
+      ]
+    };
+  }
+
+  @Get('/catalog')
+  @Render('catalog')
+  catalog() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Меню",
+      products: [
+        {
+          image:"images/cake-chocolate.jpeg",
+          product:"Шоколадный торт",
+          price: 2899
+        },
+        {
+          image:"images/cake-strawberry.jpeg",
+          product:"Клубничный торт",
+          price: 2799
+        },
+        {
+          image:"images/cake-honey.jpeg",
+          product:"Медовик",
+          price: 2999
+        },
+        {
+          image:"images/cake-berries.jpeg",
+          product:"Ежевичный торт",
+          price: 2899
+        },
+        {
+          image:"images/cake-caramel.jpeg",
+          product:"Торт \"Соленая карамель\"",
+          price: 3199
+        },
+        {
+          image:"images/cake-red-velvet.jpeg",
+          product:"Красный бархат",
+          price: 2999
+        },
+        {
+          image:"images/cake-limon.jpeg",
+          product:"Лимонный торт",
+          price: 2699
+        }
+      ]
+    };
+  }
+
+  @Get('/catalog-cakes')
+  @Render('catalog')
+  catalogCakes() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Торты",
+      products: [
+        {
+          image:"images/cake-chocolate.jpeg",
+          product:"Шоколадный торт",
+          price: 2899
+        },
+        {
+          image:"images/cake-strawberry.jpeg",
+          product:"Клубничный торт",
+          price: 2799
+        },
+        {
+          image:"images/cake-honey.jpeg",
+          product:"Медовик",
+          price: 2999
+        },
+        {
+          image:"images/cake-berries.jpeg",
+          product:"Ежевичный торт",
+          price: 2899
+        },
+        {
+          image:"images/cake-caramel.jpeg",
+          product:"Торт \"Соленая карамель\"",
+          price: 3199
+        },
+        {
+          image:"images/cake-red-velvet.jpeg",
+          product:"Красный бархат",
+          price: 2999
+        },
+        {
+          image:"images/cake-limon.jpeg",
+          product:"Лимонный торт",
+          price: 2699
+        }
+      ]
+    };
+  }
+
+  @Get('/catalog-bento')
+  @Render('catalog')
+  catalogBento() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Бенто-торты"
+    };
+  }
+
+  @Get('/catalog-cupcakes')
+  @Render('catalog')
+  catalogCupcakes() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Капкейки"
+    };
+  }
+
+  @Get('/catalog-donuts')
+  @Render('catalog')
+  catalogDonuts() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Пончики"
+    };
+  }
+
+  @Get('/catalog-macaroni')
+  @Render('catalog')
+  catalogMacaroni() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Макарони"
+    };
+  }
+
+  @Get('/catalog-drinks')
+  @Render('catalog')
+  catalogDrinks() {
+    return {
+      title: 'Каталог',
+      isCatalog: true,
+      catalog: "Напитки"
+    };
+  }
+
+  @Get('/account')
+  @Render('account')
+  account() {
+    return { title: 'Личный кабинет'};
+  }
+
+  @Post('/login')
+  @Render('account')
+  login() {
+    return {
+      title: 'Личный кабинет',
+      isAuthorized: true};
+  }
+
+  @Get('/constructor')
+  @Render('constructor')
+  construct() {
+    return { title: 'Конструктор десертов'};
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,22 @@ import { AppModule } from './app.module';
 import { Logger } from "@nestjs/common";
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import hbs = require('hbs');
+import { TimeLoadingInterceptor } from "./time.loading.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
+  app.useGlobalInterceptors(new TimeLoadingInterceptor());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views', 'pages'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+
   const port = process.env.PORT || 3000;
-  Logger.log("Application is running on: http://localhost:${port}");
+  Logger.log(`Application is running on: http://localhost:${port}`);
   await app.listen(port);
 }
 bootstrap();
